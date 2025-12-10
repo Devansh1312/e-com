07bf41f6a7f7842062cc5f903c535d02694ea29e
Devansh shah <shahdevansh13122002@gmail.com>
2025-12-10 18:28:53 +0530

    Remove unused custom filters and permission tags from templates

--- reward_admin/templatetags/permission_tags.py
+++
@@ -1,8 +0,0 @@
-# presidencyclub/templatetags/permission_tags.py
-from django import template
-
-register = template.Library()
-
-@register.filter(name='has_permission')
-def has_permission(user, permission_name):
-    return user.has_permission(permission_name)
